078e670646315837916af34c2f85bf0c5920e7bf
renzinza <130158423+justRenzinza@users.noreply.github.com>
2024-08-27 16:00:31 -0300

    Delete petcare-frontend-main directory

--- petcare-frontend-main/js/script.js
+++
@@ -1,9 +0,0 @@
-// document.querySelectorAll('.fotos-vet img').forEach(function(img) {
-//     img.addEventListener('mouseover', function() {
-//         this.classList.add('zoom');
-//     });
-
-//     img.addEventListener('mouseout', function() {
-//         this.classList.remove('zoom');
-//     });
-// });
